78b0752fedaaa8c2b6c536d1696f9049a2c0e08c
Josue Gallegos <gallegosjosue6@gmail.com>
2023-05-12 20:12:53 -0500

    complete array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    let win = record.find(({result}) => result === "W")
+        if (win){
+            return win["year"]
+        }
+}
+
